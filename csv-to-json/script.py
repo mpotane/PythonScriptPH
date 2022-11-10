76c609d81553b96b3f6b16b0faad613d7d29c24e
mpotane <1000100ten@gmail.com>
2022-11-10 23:34:52 +0800

    Refactor code for csv-to-json 🎍.

--- csv-to-json/script.py
+++ csv-to-json/script.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
 # Reading the csv file and converting it to a json file.
-df = pd.read_csv('sample.csv')
-df.to_json('sample.json')
+
+
+def main():
+    df = pd.read_csv('sample.csv')
+    df.to_json('sample.json')
+
+
+if __name__ == '__main__':
+    main()
